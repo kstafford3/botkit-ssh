053d177a936b4a0321e4c4af1dfc4c42978cd586
Kyle Stafford <kylejstafford@gmail.com>
2017-06-10 14:34:55 -0400

    Initial Commit
    Background:
    ----------
    Botkit is the api we will be adapting to work throught ssh.
    
    SSH2 is a library for creating ssh servers and clients.
    
    Actions:
    -------
    * Git Init
    * NPM Init
    * Install Botkit/SSH2
    * Install eslint
    * Create placeholder index.js with "Hello, World!" message

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // add your custom rules here
+  'rules': {
+    // enforce 2 spaces for indentation
+    "indent": ["error", 2],
+    // allow paren-less arrow functions
+    'arrow-parens': 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': (process.env.NODE_ENV === 'production' ? 'error' : 'off'),
+    // require semicolons
+    'semi': [ 'error', 'always' ],
+    // require trailing commas
+    'comma-dangle': ['error', 'always-multiline'],
+    // require space before function paren, only for anonymous and arrow methods
+    'space-before-function-paren': [ "error", {'anonymous': 'always', 'named': 'never', 'asyncArrow': 'always'} ],
+    // put linebreak before the operator
+    'operator-linebreak': ["error", "before"],
+    // I'm using literal error strings, sorry not sorry
+    'standard/no-callback-literal': ['off'],
+  }
+}
